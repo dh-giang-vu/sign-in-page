a4fd73217d626e3cb08b2c577540bcc81a18134a
Giang <dhgiang.vu@gmail.com>
2024-08-03 12:57:02 +1000

    FormComponent

--- src/Components/FormComponent.tsx
+++ src/Components/FormComponent.tsx
@@ -1,2 +1,18 @@
-function FormComponent() {}
+import Textbox from "./Textbox";
+
+interface FormComponentProps {
+  title: string,
+  example: string,
+  inputType?: string
+}
+
+function FormComponent({ title, example, inputType="text" }: FormComponentProps) {
+  return (
+    <div>
+      <Textbox color="black" fontSize={15} fontWeight="bold" content={title}/>
+      <Textbox color="black" fontSize={15} content={example}/>
+      <input type={inputType} />
+    </div>
+  );
+}
 export default FormComponent;
