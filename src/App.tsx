20a6b8f32e48720abb3a32a8897c4f4a5f00e110
Giang <dhgiang.vu@gmail.com>
2024-08-03 13:00:15 +1000

    App skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Logo from "./Components/Logo";
 import Textbox from "./Components/Textbox";
+import FormComponent from "./Components/FormComponent";
+import Button from "./Components/Button";
 import './App.css';
 
 function App() {
@@ -8,6 +10,13 @@ function App() {
       <Textbox color="black" fontSize={20} content="Sign in with your University of Melbourne account to access Student Portal" />
       <Logo />
       <Textbox color="black" fontSize={20} fontWeight="bold" content="Log In" />
+
+      <div className="form">
+        <FormComponent title="Username" example="e.g. jsmith (not email address)"/>
+        <FormComponent title="Password" example="e.g. jsmith123" inputType="password"/>
+      </div>
+
+      <Button />
     </>
   )
 }
