9bc4376f5955ae0a518d7acf5bc0a68f4a4e3c90
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-08-03 14:22:42 +1000

    deploy fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://dh-giang-vu.github.io/sign-in-page",
 })
