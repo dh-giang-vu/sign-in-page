0cfad04e493e7312a5f6ed6fb2c40cbdfcab05c6
Giang <dhgiang.vu@gmail.com>
2024-08-03 12:19:56 +1000

    Textbox component

--- src/Components/Textbox.tsx
+++ src/Components/Textbox.tsx
@@ -1,2 +1,19 @@
-function Textbox() {}
+interface TextBoxProps {
+  color: string,
+  fontSize: number,
+  fontWeight?: string,
+  content: string
+}
+
+function Textbox({ color, fontSize, fontWeight="normal", content }: TextBoxProps) {
+  const tbStyle = {
+    color: color,
+    fontSize: fontSize + "px",
+    fontWeight: fontWeight
+  };
+
+  return (
+    <p style={tbStyle}>{content}</p>
+  )
+}
 export default Textbox;
