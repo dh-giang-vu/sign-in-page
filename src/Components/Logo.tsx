9901b9c15a4024078f77124500548fc38b730b42
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-08-03 14:29:23 +1000

    image issues fix

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,6 +1,6 @@
 function Logo() {
   return (
-    <img src="src\assets\UMLogo.svg" style={{width: 200+"px", height: 200 + "px"}}></img>
+    <img src="sign-in-page\src\assets\UMLogo.svg" style={{width: 200+"px", height: 200 + "px"}}></img>
   );
 }
 export default Logo;
