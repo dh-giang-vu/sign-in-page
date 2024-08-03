867e7907f8f931335c6423d0dbe0f8776b5d69bd
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-08-03 14:36:50 +1000

    img tag fix 2

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,6 +1,8 @@
+import UMLogo from "../assets/UMLogo.svg"
+
 function Logo() {
   return (
-    <img src="sign-in-page\src\assets\UMLogo.svg" style={{width: 200+"px", height: 200 + "px"}}></img>
+    <img src={UMLogo} style={{width: 200+"px", height: 200 + "px"}}></img>
   );
 }
 export default Logo;
